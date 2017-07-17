6ea6cec667276e3d839bf0fe6d77b17c8ca8f5e5
Andrew Teixeira <teixeira@broadinstitute.org>
2017-07-17 12:51:11 -0400

    * Add options to distribute a dhparam file in a site * Spacing cleanup in .pp files * Fix concat and stdlib versions in .fixtures.yml (Puppet 3) * Clean up spacing in spec files * Remove unneeded settings from pre conditions in spec tests * Reorder and fill in missing option documentation for certs::site in README * Update Rakefile to make tests faster when run multiple times * Fix "ensure" in site.pp so the code is cleaner * Add spec tests for new dhparam options * Fix author in metadata.json as lint tests apparently disallow array and require string * Increment the version in metadata * Use lower Ruby version for Puppet 3 tests

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -2,65 +2,62 @@ require 'spec_helper'
 
 describe 'certs', :type => :class do
 
-  [ 'Debian', 'FreeBSD', 'Gentoo', 'RedHat' ].each do |osfamily|
+    [ 'Debian', 'FreeBSD', 'Gentoo', 'RedHat' ].each do |osfamily|
 
-    context "on #{osfamily}" do
+        context "on #{osfamily}" do
 
-      if osfamily == 'Debian'
-        let(:facts) { {
-          :osfamily               => 'Debian',
-          :operatingsystem        => 'Debian',
-          :lsbdistid              => 'Debian',
-          :lsbdistcodename        => 'wheezy',
-          :operatingsystemrelease => '7.3',
-          :operatingsystemmajrelease => '7',
-        } }
+            if osfamily == 'Debian'
+                let(:facts) {{
+                    :osfamily               => 'Debian',
+                    :operatingsystem        => 'Debian',
+                    :lsbdistid              => 'Debian',
+                    :lsbdistcodename        => 'wheezy',
+                    :operatingsystemrelease => '7.3',
+                    :operatingsystemmajrelease => '7',
+                }}
 
-        context 'with defaults for all parameters' do
-          it { is_expected.to contain_class('certs') }
+                context 'with defaults for all parameters' do
+                    it { is_expected.to contain_class('certs') }
+                end
+            end
+
+            if osfamily == 'FreeBSD'
+                let(:facts) {{
+                    :osfamily => osfamily,
+                    :operatingsystem => 'FreeBSD',
+                    :operatingsystemrelease => '10.0-RELEASE-p18',
+                    :operatingsystemmajrelease => '10',
+                }}
+
+                context 'with defaults for all parameters' do
+                    it { is_expected.to contain_class('certs') }
+                end
+            end
+
+            if osfamily == 'Gentoo'
+                let(:facts) {{
+                    :osfamily => osfamily,
+                    :operatingsystem => 'Gentoo',
+                    :operatingsystemrelease => '3.16.1-gentoo',
+                }}
+
+                context 'with defaults for all parameters' do
+                    it { is_expected.to contain_class('certs') }
+                end
+            end
+
+            if osfamily == 'RedHat'
+                let(:facts) {{
+                    :osfamily => osfamily,
+                    :operatingsystem => 'RedHat',
+                    :operatingsystemrelease => '7.2',
+                    :operatingsystemmajrelease => '7',
+                }}
+
+                context 'with defaults for all parameters' do
+                    it { is_expected.to contain_class('certs') }
+                end
+            end
         end
-      end
-
-      if osfamily == 'FreeBSD'
-        let(:facts) { {
-          :osfamily => osfamily,
-          :operatingsystem => 'FreeBSD',
-          :operatingsystemrelease => '10.0-RELEASE-p18',
-          :operatingsystemmajrelease => '10',
-        } }
-
-        context 'with defaults for all parameters' do
-          it { is_expected.to contain_class('certs') }
-        end
-
-      end
-
-      if osfamily == 'Gentoo'
-        let(:facts) { {
-          :osfamily => osfamily,
-          :operatingsystem => 'Gentoo',
-          :operatingsystemrelease => '3.16.1-gentoo',
-        } }
-
-        context 'with defaults for all parameters' do
-          it { is_expected.to contain_class('certs') }
-        end
-
-      end
-
-      if osfamily == 'RedHat'
-        let(:facts) { {
-          :osfamily => osfamily,
-          :operatingsystem => 'RedHat',
-          :operatingsystemrelease => '7.2',
-          :operatingsystemmajrelease => '7',
-        } }
-
-        context 'with defaults for all parameters' do
-          it { is_expected.to contain_class('certs') }
-        end
-      end
-
     end
-  end
 end
